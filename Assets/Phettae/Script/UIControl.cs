e61a23d3580844b5fd98cf51ef113f9afe88c227
DESKTOP-5ATEQMQ\admin <Phonethavixay@gmail.com>
2026-06-06 11:06:34 +0700

    Add UI Navigation

---
+++ Assets/Phettae/Script/UIControl.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public GameObject uiPanel;
+    public bool _isPanelActive;
+
+    private void Start()
+    {
+        uiPanel.SetActive(false);
+        _isPanelActive = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && !_isPanelActive)
+        {
+            uiPanel.SetActive(true);
+            _isPanelActive = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Escape) && _isPanelActive)
+        {
+            uiPanel.SetActive(false);
+            _isPanelActive = false;
+        }
+    }
+}
